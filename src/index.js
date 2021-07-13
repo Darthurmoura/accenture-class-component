ec30060439ae7beeeeeb765a1e6d4a67a96c4123
Daniel Moura <darthur.moura@gmail.com>
2021-07-13 18:57:38 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+  state = {
+    texto: 'AEHOOOO',
+  }
+
+  handleClick() {
+    alert('Alerta ativado!');
+  }
+
+  handleTextChange(e) {
+    this.setState({
+      texto: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Dale, {this.props.stuff.name}.</h1>
+        <h2>Tu tem {this.props.stuff.age} anos.</h2>
+        <input type='text' onChange={this.handleTextChange.bind(this)}></input>
+        <button onClick={this.handleClick.bind(this)}>Exibir alerta</button>
+        <h3>{this.state.texto}</h3>
+      </div>
+    )
+  }
+}
+
+const stuff = {
+  name: 'Daniel',
+  age: 35,
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App stuff={stuff} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
